67da37875ffffd9ffed43a1fdbf7fcef8649674a
Samar1110 <98534735+Samar1110@users.noreply.github.com>
2022-08-12 23:49:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -28,7 +28,31 @@ export default function App() {
       <img src={image4} onDragStart={handleDragStart} alt=""/>
     </AliceCarousel>
     </div>
-
+ <footer class="p-4 bg-white mt-6 shadow md:px-6 md:py-8 dark:bg-teal-500">
+    <div class="sm:flex sm:items-center sm:justify-between">
+        <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0">
+            <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-8" alt="Flowbite Logo" />
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Phantom</span>
+        </a>
+        <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-white">
+            <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6  dark:text-white ">About</a>
+            </li>
+            <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6  dark:text-white">Privacy Policy</a>
+            </li>
+            <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6  dark:text-white ">Licensing</a>
+            </li>
+            <li>
+                <a href="#" class="hover:underline  dark:text-white">Contact</a>
+            </li>
+        </ul>
+    </div>
+    <hr class="my-6 border-white-200 sm:mx-auto dark:border-white-700 lg:my-8  dark:text-white" />
+    <span class="block text-sm text-whit sm:text-center  dark:text-white">© 2022 <a href="#" class="hover:underline  dark:text-white">Phantom™</a>. All Rights Reserved.
+    </span>
+</footer>
     </>
     
   );
